4e6f66a2f444124187adc122a779d86a29e84b9e
tarrenj <tarrenj@users.noreply.github.com>
2014-11-20 12:30:20 -0500

    Update Measure.py

--- Measure.py
+++ Measure.py
@@ -1,1 +1,10 @@
-# Comment
+Class Measure:
+  'Each section of the song'
+  
+  # Create a list of connections
+  connections = []
+  
+  def __init__(self, audio, pos):
+    # set the objects audio and position
+    self.audio = audio
+    self.pos = pos
