2e61f7f42ebc1dc1e0f594807f7813a404954ead
tarrenj <tarrenj@users.noreply.github.com>
2014-11-20 12:30:14 -0500

    Update Song.py

--- Song.py
+++ Song.py
@@ -1,1 +1,13 @@
-# Comment
+import Measure
+
+Class Song:
+  'The whole song file'
+  
+  # Create a list of measures
+  measureList = []
+  measureCount = 
+  
+  # Create a measure for each soundbyte
+  for x in range(soundBytes):
+    x = Measure(soundByte, x)
+    measureList.append(x)
